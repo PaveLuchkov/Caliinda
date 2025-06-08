3d71cc75c0f5047aeda8ea07647b89bf604dacf2
PaveLuchkov <sliderlaad222@gmail.com>
2025-06-08 13:58:04 +0500

    добавил state с информацией о пользователе (болванка)

--- src/session.py
+++ src/session.py
@@ -1,13 +1,12 @@
-import logging
+from datetime import datetime
 from google.adk.sessions import InMemorySessionService, Session as ADKSession
 
 import src.shared.config as cfg 
 
 session_service = InMemorySessionService()
 
-APP_NAME = "caliinda"
-USER_ID_FOR_SESSION = "112812348232829088110"
-SESSION_ID = "session_001"
+APP_NAME, USER_ID_FOR_SESSION, SESSION_ID = "caliinda", "112812348232829088110", "session_001"
+current_user_time, user_timezone, user_prefered_calendar = datetime.now().replace(microsecond=0), "Asia/Yekaterinburg", "primary"
 
 adk_user_session: ADKSession = session_service.get_session(
     app_name=APP_NAME, user_id=cfg.TEST_USER_GOOGLE_ID, session_id=SESSION_ID
@@ -16,5 +15,10 @@ if not adk_user_session:
     adk_user_session = session_service.create_session(
         app_name=APP_NAME,
         user_id=cfg.TEST_USER_GOOGLE_ID,
-        session_id=SESSION_ID
+        session_id=SESSION_ID,
+        state={
+            "current_user_time": current_user_time,
+            "user_timezone": user_timezone,
+            "user_prefered_calendar": user_prefered_calendar,
+        },
     )
