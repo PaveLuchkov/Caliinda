4474ed4095b0b85bb45f8845fd26e0549f86e6ce
PaveLuchkov <sliderlaad222@gmail.com>
2025-06-13 12:26:27 +0500

    адаптированы под инит ипорты и доавблены TODO

--- src/sub_agents/quick_patcher/agent.py
+++ src/sub_agents/quick_patcher/agent.py
@@ -1,12 +1,9 @@
 from google.adk.agents import Agent
-
 from ...shared import config as cfg
+from . import prompt
+from ...tools import calendarActionTools
 
 MODEL = cfg.MODEL
-from . import prompt
-from src.tools.calendar_tools import calendarActionTools
-
-
 
 _calendar_handler = Agent(
     name="Calendar_Agent",
@@ -14,6 +11,7 @@ _calendar_handler = Agent(
     description=(
         "Agent who can create delete and edit calendar events"
     ),
-    instruction=prompt.QUICK_PATCHER,
-    tools=[calendarActionTools]
+    instruction=prompt.QUICK_PATCHER, 
+    tools=[calendarActionTools] #TODO надо добавить тул просмотра календаря - агента. Указать агента в промпте
+    # TODO для нового агента нужно добавить обработку состояния сессии и обновление последних полученных данных.
 )
